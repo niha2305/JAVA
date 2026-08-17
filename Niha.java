48f7bda552ac3f4cb7f8e60648fcdc54f5c8046c
J Niha Fathima <jnihafathima@gmail.com>
2026-08-17 20:31:35 +0530

    Add files via upload

---
+++ Niha.java
@@ -0,0 +1,19 @@
+public class Niha
+{
+    public static void main(String[] args )
+    {
+        int a=10,b=20,c=40;
+     if(a>b && a>c)
+     {
+        System.out.println("A is greater");
+     }
+     if(b>a && b>c)
+     {
+        System.out.println("B is greater");
+     }
+     if(c>a && c>b)
+     {
+        System.out.println("C is greater");
+     }
+    }
+}
