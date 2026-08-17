117ecfa2e24ce4c24ad66f9b4f91b1c3ff08036d
J Niha Fathima <jnihafathima@gmail.com>
2026-08-17 20:31:02 +0530

    Add files via upload

---
+++ Nameagecity.java
@@ -0,0 +1,9 @@
+public class Nameagecity
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is J.niha fathima");
+        System.out.println("I am 19 yrs old.");
+        System.out.println("I am from Palamaner");
+    }
+}
