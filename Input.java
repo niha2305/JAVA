ddcd189abfe441d5db13bdaa388e8aeb581c9e5e
J Niha Fathima <jnihafathima@gmail.com>
2026-08-17 20:29:49 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Input
+{
+public  static void main(String[] args)
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+System.out.println("give and b values :");
+a=sc.nextInt();
+b=sc.nextInt();
+System.out.print(a*b);
+sc.close();
+}
+}
